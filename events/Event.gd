8714bf0526fccced39458ca4eedabc64861ef30d
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2020-02-23 11:09:39 +0100

    First implementation of outcome display

--- events/Event.gd
+++ events/Event.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
 signal selected_option
+signal outcome_continue
 
 var current_opacity = 0
-var option_nodes = []
+var option_nodes = {}
+var outcome_node = null
 onready var container = $ColorRect/MarginContainer/VBoxContainer
 
+func _disable_link(link_button):
+	link_button.underline = LinkButton.UNDERLINE_MODE_NEVER
+	link_button.add_color_override("font_color", Color(0.4, 0.4, 0.6, 1))
+	link_button.add_color_override("font_color_hover", Color(0.4, 0.4, 0.6, 1))
+	
 func spawn(event, title, text, options):
 	var title_element = container.find_node("Title")
 	var text_element = container.find_node("Text")
@@ -22,30 +29,47 @@ func spawn(event, title, text, options):
 		var option_descr = options[option]["description"]
 
 		option_node.text = "> " + option_descr
-		option_node.margin_left = text_element.margin_left
-		option_node.margin_top = y_pos
 		
 		if option_enabled:
 			option_node.underline = LinkButton.UNDERLINE_MODE_ON_HOVER
 			option_node.connect("pressed", self, "_on_selected_option", [event, option_value])
 		else:
-			option_node.underline = LinkButton.UNDERLINE_MODE_NEVER
-			option_node.add_color_override("font_color", Color(0.4, 0.4, 0.6, 1))
-			option_node.add_color_override("font_color_hover", Color(0.4, 0.4, 0.6, 1))
+			_disable_link(option_node)
 		
-		option_nodes.append(option_node)
+		option_nodes[option] = option_node
 
 		container.add_child(option_node)
-		
-		y_pos += option_node.rect_size.y + 10
 
 	show()
 	
+func show_outcome(option, outcome, next_event):
+	var selected_option_text = option_nodes[option].text
+	outcome_node = load("res://events/Outcome.tscn").instance()
+	var option_node = outcome_node.find_node("Option")
+	
+	for option in option_nodes:
+		container.remove_child(option_nodes[option])
+	option_nodes = {}
+	
+	option_node.text = selected_option_text
+	_disable_link(option_node)
+	outcome_node.find_node("Description").text = outcome
+	outcome_node.find_node("Continue").connect("pressed", self, "_on_continue_pressed", [next_event])
+	
+	container.add_child(outcome_node)
+
+	
 func discard():
-	for option_node in option_nodes:
-		container.remove_child(option_node)
+	for option in option_nodes:
+		container.remove_child(option_nodes[option])
+		
+	if outcome_node:
+		container.remove_child(outcome_node)
 	
 	hide()
 
 func _on_selected_option(event, option):
 	emit_signal("selected_option", event, option)
+	
+func _on_continue_pressed(next_event):
+	emit_signal("outcome_continue", next_event)
